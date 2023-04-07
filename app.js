f2ecdca5013c817a20ce603ae7a8bf4f97333978
KimEunA97 <kimeuna9712@gmail.com>
2023-04-07 10:53:42 +0900

    아기 서버

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+import http from 'http'
+
+
+const server = http.createServer(function (req, res) {
+
+
+
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end();
+
+
+})
+
+server.listen(3050, function (error) {
+
+
+  if(error) {
+
+    console.log("서버 에러");
+
+  }
+
+  else{
+
+    console.log("서버 구동 중...");
+
+  }
+
+
+})
+
